c4e2672b8019a76c21c304c7ecc6b8098275ef04
sivanmah <sivansm@stud.ntnu.no>
2022-04-06 16:12:42 +0200

    boilerplate code

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
